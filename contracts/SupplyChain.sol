f968af179290056e98cd41efd69147df069054aa
miles <miles1618@gmail.com>
2021-09-21 05:56:34 -0500

    Contract events, modifiers

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -4,5 +4,49 @@ pragma solidity >=0.4.24;
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 contract SupplyChain is ERC721 {
-    constructor() public {}
+    address owner;
+    uint skuCount;
+    enum State {Plant, Harvest, Process, Grade, Export, Roast, Ship, Purchase}
+
+    struct Coffee {
+        uint sku;
+        uint price;
+        State state;
+        address farmer;
+        address inspector;
+        address distributor;
+        address payable consumer;
+    }
+
+    mapping(uint => Coffee) coffees;
+    event Planted(uint skuCount);
+    event Harvested(uint sku);
+    event Processed(uint sku);
+    event Graded(uint sku);
+    event Exported(uint sku);
+    event Roasted(uint sku);
+    event Shipped(uint sku);
+    event Purchased(uint sku);
+
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier paidEnough(uint _price){
+        require(msg.value >= _price);
+        _;
+    }
+
+    modifier checkValue(uint _sku){
+        _;
+        uint _price = coffees[_sku].price;
+        uint amountToRefund = msg.value - _price;
+        coffees[_sku].consumer.transfer(amountToRefund);
+    }
+
+    constructor() public {
+        owner = msg.sender;
+        skuCount = 0;
+    }
 }
